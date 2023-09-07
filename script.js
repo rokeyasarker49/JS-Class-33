eeda161c75433e92d4ac130f5e4d1a2c7ee4636f
Rokeya Sarker <133252712+rokeyasarker49@users.noreply.github.com>
2023-09-07 12:15:10 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,115 @@
+/* function stopWatch() {
+  let counter = 0;
+
+  return function(){
+
+    counter++;
+    return counter;
+  }
+
+}
+let clockOne = stopWatch();
+console.log(clockOne);*/
+
+
+
+/* function stopWatch() {
+  let counter = 0;
+
+  return function(){
+
+    counter++;
+    return counter;
+  }
+
+}
+let clockOne = stopWatch()();
+console.log(clockOne); */
+
+
+
+/* function stopWatch() {
+  let counter = 0;
+
+  return function(){
+
+    counter++;
+    return counter;
+  }
+
+}
+let clockOne = stopWatch();
+console.log(clockOne()); */
+
+
+
+/* function stopWatch() {
+  let counter = 0;
+
+  return function(){
+
+    counter++;
+    return counter;
+  }
+
+}
+let clockOne = stopWatch();
+console.log(clockOne());
+console.log(clockOne());
+console.log(clockOne());
+console.log(clockOne());
+console.log(clockOne());*/
+
+
+
+
+/* function stopWatch() {
+  let counter = 0;
+
+  return function(){
+
+    counter++;
+    return counter;
+  }
+
+}
+let clockOne = stopWatch();
+let clockTwo = stopWatch();
+console.log(clockOne());
+console.log(clockOne());
+console.log(clockOne());
+console.log(clockTwo());
+console.log(clockTwo());*/
+
+
+
+
+/* function stopWatch() {
+  let counter = 0;
+
+  return function(){
+
+    counter++;
+    return counter;
+  }
+
+}
+let clockOne = stopWatch();
+let clockTwo = stopWatch();
+console.log(clockOne());
+console.log(clockTwo());
+console.log(clockOne());
+console.log(clockOne());
+console.log(clockTwo());
+console.log(clockTwo());*/
+
+
+// CallBack Is Here ===============================
+/* function wellComeMsg(Name, GH){
+
+  console.log(Name);
+}
+wellComeMsg("Rokeya Sarker");
+Ans Is == Ayman Sadiq */
+
+
